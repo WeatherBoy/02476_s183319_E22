aaa9291abff58303afb056926428a8d54557b61c
WeatherBoy <dafloxi@gmail.com>
2023-01-12 13:58:09 +0100

    Push with cookiecutter

---
+++ Main_project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exercises in 02476',
+    author='Felix',
+    license='MIT',
+)
